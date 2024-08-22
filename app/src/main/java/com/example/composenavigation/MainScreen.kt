8e15095b5ac4c5983dc2fc34e5857de6046ad14f
AmirahmadAdibi <amirahmadadibi@gmail.com>
2024-08-22 18:52:23 +0330

    E23 compelte design mainscreen

--- app/src/main/java/com/example/composenavigation/MainScreen.kt
+++ app/src/main/java/com/example/composenavigation/MainScreen.kt
@@ -1,35 +1,96 @@
 package com.example.composenavigation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+
+    var username by remember {
+        mutableStateOf("")
+    }
+
+    var family by remember {
+        mutableStateOf("")
+    }
+
+    var ID by remember {
+        mutableStateOf("")
+    }
+
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("MainScreen") })
+            CenterAlignedTopAppBar(
+                title = { Text("Registration Screen") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Black,
+                    titleContentColor = Color.White
+                )
+            )
         }
     ) { contentPadding ->
-        Column(modifier = Modifier.padding(contentPadding)) {
-            Text(text = "AmirahmadAdibi1")
-            Text(text = "AmirahmadAdibi2")
-            Text(text = "AmirahmadAdibi3")
-            Text(text = "AmirahmadAdibi4")
-            Text(text = "AmirahmadAdibi5")
-            Text(text = "AmirahmadAdibi6")
-            Text(text = "AmirahmadAdibi7")
-            Text(text = "AmirahmadAdibi8")
-            Text(text = "AmirahmadAdibi9")
-            Text(text = "AmirahmadAdibi10")
-            Text(text = "AmirahmadAdibi11")
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center
+
+        ) {
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = username,
+                label = { Text(text = "username") },
+                onValueChange = { username = it })
+            Spacer(modifier = Modifier.height(20.dp))
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = family,
+                label = { Text(text = "username") },
+                onValueChange = { username = it })
+            Spacer(modifier = Modifier.height(20.dp))
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = ID,
+                label = { Text(text = "username") },
+                onValueChange = { username = it })
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(5.dp),
+                onClick = {
+
+                }) {
+                Text(text = "Register", fontSize = 20.sp)
+            }
         }
     }
 }
